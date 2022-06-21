0a7712099e052c004a67fc12dfaee29b74520950
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-06-21 10:18:25 +0300

    working with firebase and currently setting up user Documents

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,39 @@
+import { initializeApp } from 'firebase/app';
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDBNjZzdA2KPmSDZLzaO32SoCAOLIEnXzM',
+	authDomain: 'crwn-clothing-db-d37e2.firebaseapp.com',
+	projectId: 'crwn-clothing-db-d37e2',
+	storageBucket: 'crwn-clothing-db-d37e2.appspot.com',
+	messagingSenderId: '1069760245417',
+	appId: '1:1069760245417:web:e88c46a27ff885c0e7c3b8',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot.exists)
+};
