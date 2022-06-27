bff63a81f3b3d1125ec6f69d71795fa2a3a83847
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-06-27 19:25:48 +0300

    started working on firebase database storage

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,11 +1,12 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const ProductsContext = createContext({});
 
 export const ProductsProvider = ({ children }) => {
-	const [products, setProducts] = useState(PRODUCTS);
+	const [products, setProducts] = useState([]);
+
 	const value = { products };
 
 	return (
